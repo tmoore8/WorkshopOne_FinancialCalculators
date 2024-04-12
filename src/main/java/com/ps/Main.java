29279fdd00c2c23cd88a2ad252b376dc43330c41
tmoore8 <107230156+tmoore8@users.noreply.github.com>
2024-04-12 16:00:42 -0400

    accepts values

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+
         //prompts user to input which calculator they want
         System.out.println("Which Calculator would you like?");
         //receives input
-        Scanner whichCalc = new Scanner(System.in);
-        System.out.println("Mortgage, Future Value, or Present Value?");
-        String response = whichCalc.nextLine();
+        System.out.println("(M)ortgage, (F)uture Value, or (P)resent Value?");
+        String response = scanner.nextLine();
+        if (response.equalsIgnoreCase("m")) {
+         mortgageCalc();
 
+        }
+    }
+    public static double mortgageCalc(){
+        System.out.println("Enter Principal");
+        double principal = scanner.nextFloat();
+        System.out.println("Enter Interest Rate");
+        double iRate = scanner.nextFloat();
+        System.out.println("Enter Length");
+        int length = scanner.nextInt();
+        return 0;
     }
 
 }
+
