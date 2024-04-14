a5b05827deb4297e8ee82c0c35f691779240fcf0
tmoore8 <107230156+tmoore8@users.noreply.github.com>
2024-04-14 18:44:41 -0400

    second static method added(future value)[floating-point precision error]

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -3,6 +3,7 @@ package com.ps;
 import java.util.Scanner;
 
 
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
@@ -16,6 +17,7 @@ public class Main {
             mortgageCalc();
         }
         else if (response.equalsIgnoreCase("f")){
+            futureCalc();
         }
         else if (response.equalsIgnoreCase("p")){
 
@@ -37,15 +39,25 @@ public class Main {
         double interestPaid = totalPayment - principal;
         System.out.println("Your expected monthly payment is: " + String.format("%.2f",monthlyPayment));
         System.out.println("Your total interest paid is: " + String.format("%.2f",interestPaid));
-
-
-
-
-
-
-
-
         return monthlyPayment;
+    }
+    //second static method (future value)
+    public static double futureCalc(){
+            System.out.println("Enter Deposit");
+            double deposit = scanner.nextFloat();
+            System.out.println("Enter Interest Rate");
+            double iRate = scanner.nextFloat();
+            System.out.println("Enter Length");
+            int length = scanner.nextInt();
+
+            double dailyiRate = iRate / 100 / 365;
+            int lengthDays = length * 365;
+            int compoundPeriod = 365 * length;
+            double futureValue = deposit * Math.pow(1+ dailyiRate, lengthDays);
+            double interestEarned = futureValue - deposit;
+            System.out.println("Your expected future value is: " + String.format("%.2f", futureValue));
+            System.out.println("Your total interest earned is: " + String.format("%.2f", interestEarned));
+        return futureValue;
     }
 }
 
